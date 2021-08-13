b7dad9baacf83c79d020614ae3b5ab46af91ee64
michaelpaolo <michaelpaolo@gmail.com>
2021-08-13 20:27:09 +0000

    commit de cambios en botones

--- webapp/controller/mainView.controller.js
+++ webapp/controller/mainView.controller.js
@@ -1,19 +1,26 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.invoices.controller.mainView", {
-			onInit: function () {
-            const oJsonModel = new sap.ui.model.json.JSONModel();
-            const oView = this.getView();
-            oJsonModel.loadData("./model/SelectionScreenMenu.json");
-            oView.setModel(oJsonModel,"selectionScreen")
+        return Controller.extend("logaligroup.invoices.controller.mainView", {
+            onInit: function () {
+                const oJsonModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJsonModel.loadData("./model/SelectionScreenMenu.json");
+                oView.setModel(oJsonModel, "selectionScreen")
+            },
+            onFilter: function(oEvent) {
 
-			}
-		});
-	});
+            },
+            onClearFilter: function(){
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/ShipName","");
+                oModelSelScreen.setProperty("/CountryKey","");
+            }
+        });
+    });
